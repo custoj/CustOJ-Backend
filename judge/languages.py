b82658518a88733b55eba9a9a4383ffb46b7865d
ccw630 <ccw630@gmail.com>
2020-10-11 13:29:37 +0800

    modify lang

--- judge/languages.py
+++ judge/languages.py
@@ -75,7 +75,7 @@ int main() {
         "max_cpu_time": 10000,
         "max_real_time": 20000,
         "max_memory": 1024 * 1024 * 1024,
-        "compile_command": "/usr/bin/g++ -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c++14 {src_path} -lm -o {exe_path}",
+        "compile_command": "/usr/bin/g++ -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c++17 {src_path} -lm -o {exe_path}",
     },
     "run": {
         "command": "{exe_path}",
@@ -90,7 +90,7 @@ _cpp_lang_spj_compile = {
     "max_cpu_time": 10000,
     "max_real_time": 20000,
     "max_memory": 1024 * 1024 * 1024,
-    "compile_command": "/usr/bin/g++ -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c++14 {src_path} -lm -o {exe_path}"
+    "compile_command": "/usr/bin/g++ -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c++17 {src_path} -lm -o {exe_path}"
 }
 
 _cpp_lang_spj_config = {
@@ -125,30 +125,6 @@ _java_lang_config = {
     }
 }
 
-
-_py2_lang_config = {
-    "template": """//PREPEND BEGIN
-//PREPEND END
-
-//TEMPLATE BEGIN
-//TEMPLATE END
-
-//APPEND BEGIN
-//APPEND END""",
-    "compile": {
-        "src_name": "solution.py",
-        "exe_name": "solution.pyc",
-        "max_cpu_time": 5000,
-        "max_real_time": 10000,
-        "max_memory": 256 * 1024 * 1024,
-        "compile_command": "/usr/bin/python -m py_compile {src_path}",
-    },
-    "run": {
-        "command": "/usr/bin/python {exe_path}",
-        "seccomp_rule": "general",
-        "env": default_env
-    }
-}
 _py3_lang_config = {
     "template": """//PREPEND BEGIN
 //PREPEND END
@@ -160,11 +136,11 @@ _py3_lang_config = {
 //APPEND END""",
     "compile": {
         "src_name": "solution.py",
-        "exe_name": "__pycache__/solution.cpython-35.pyc",
+        "exe_name": "solution_e.py",
         "max_cpu_time": 5000,
         "max_real_time": 10000,
         "max_memory": 256 * 1024 * 1024,
-        "compile_command": "/usr/bin/python3 -m py_compile {src_path}",
+        "compile_command": "/bin/cp {src_path} {exe_path}",
     },
     "run": {
         "command": "/usr/bin/python3 {exe_path}",
@@ -276,13 +252,12 @@ _scala_lang_config = {
 
 languages = [
     {"config": _c_lang_config, "spj": {"compile": _c_lang_spj_compile, "config": _c_lang_spj_config},
-     "name": "C", "description": "GCC 5.4", "content_type": "text/x-csrc"},
+     "name": "C", "description": "GCC 9.3.0", "content_type": "text/x-csrc"},
     {"config": _cpp_lang_config, "spj": {"compile": _cpp_lang_spj_compile, "config": _cpp_lang_spj_config},
-     "name": "C++", "description": "G++ 5.4", "content_type": "text/x-c++src"},
-    {"config": _java_lang_config, "name": "Java", "description": "OpenJDK 1.8", "content_type": "text/x-java"},
-    {"config": _py2_lang_config, "name": "Python2", "description": "Python 2.7", "content_type": "text/x-python"},
-    {"config": _py3_lang_config, "name": "Python3", "description": "Python 3.5", "content_type": "text/x-python"},
-    {"config": _js_lang_config, "name": "JavaScript", "description": "Node 8.16.1", "content_type": "text/javascript"},
+     "name": "C++", "description": "G++ 9.3.0", "content_type": "text/x-c++src"},
+    {"config": _java_lang_config, "name": "Java", "description": "OpenJDK 11.0.7", "content_type": "text/x-java"},
+    {"config": _py3_lang_config, "name": "Python3", "description": "Python 3.8.2", "content_type": "text/x-python"},
+    {"config": _js_lang_config, "name": "JavaScript", "description": "Node.js v12.18.2", "content_type": "text/javascript"},
     {"config": _kotlin_lang_config, "name": "Kotlin", "description": "Kotlin 1.3.50", "content_type": "text/x-kotlin"},
     {"config": _scala_lang_config, "name": "Scala", "description": "Scala 2.13.0", "content_type": "text/x-scala"},
     {"config": _bf_lang_config, "name": "brainfuck", "description": "An interesting language", "content_type": "text/x-brainfuck"}
